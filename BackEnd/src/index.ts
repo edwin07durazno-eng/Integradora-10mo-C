2b54e1660e0805d3f161655d512886c68d7f6da1
Uriel Barrera Sánchez <barrerasanchezu@gmail.com>
2025-09-21 11:20:18 -0600

    fix(CORS): Error de manejo de CORS

--- BackEnd/src/index.ts
+++ BackEnd/src/index.ts
@@ -3,14 +3,20 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth';
 import sequelize from './config/database';
 import User from './models/User';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
 app.use(json());
 app.use('/api/auth', authRoutes);
+
 
 const connectDB = async () => {
     try {
--- BackEnd/src/routes/auth.ts
+++ BackEnd/src/routes/auth.ts
@@ -1,8 +1,14 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import { login, refresh, getMe } from '../controllers/authController';
 import { authenticateToken } from '../middleware/authMiddleware';
 
 const router = Router();
+
+// ✅ Ruta de prueba: GET /api/auth
+router.get('/', (req: Request, res: Response) => {
+    res.json({ mensaje: 'Ruta /api/auth funcionando correctamente' });
+});
+
 router.post('/login', login);
 router.post('/refresh', refresh);
 router.get('/me', authenticateToken, getMe);
